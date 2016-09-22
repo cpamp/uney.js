2826146e80f7bb037f20d6760f669e98eb85b588
Clint Pamperin <c.pamperin21@gmail.com>
2016-09-21 22:16:04 -0500

    refactor: arguments

--- src/array.js
+++ src/array.js
@@ -2,56 +2,59 @@ var $_ = (function($_) {
     'use strict';
 
     $_.findMax = function() {
-        if(arguments === void 0) { return; }
+        var a = arguments;
+        if(a === void 0) { return; }
         var max = -Infinity;
-        for(var i = 0; i < arguments.length; i++) {
-            if($_.isArray(arguments[i])) {
-                for(var j = 0; j < arguments[i].length; j++) {
-                    if($_.isArray(arguments[i][j])) {
-                        var val = $_.findMax(arguments[i][j]);
+        for(var i = 0; i < a.length; i++) {
+            if($_.isArray(a[i])) {
+                for(var j = 0; j < a[i].length; j++) {
+                    if($_.isArray(a[i][j])) {
+                        var val = $_.findMax(a[i][j]);
                         if(val > max) {
                             max = val;
                         }
                     } else {
-                        if(arguments[i][j] > max) {
-                            max = arguments[i][j];
+                        if(a[i][j] > max) {
+                            max = a[i][j];
                         }
                     }
                 }
-            } else if(arguments[i] > max) {
-                max = arguments[i];
+            } else if(a[i] > max) {
+                max = a[i];
             }
         }
         return max;
     };
 
     $_.findMin = function() {
-        if(arguments === void 0) { return; }
+        var a = arguments;
+        if(a === void 0) { return; }
         var min = Infinity;
-        for(var i = 0; i < arguments.length; i++) {
-            if($_.isArray(arguments[i])) {
-                for(var j = 0; j < arguments[i].length; j++) {
-                    if($_.isArray(arguments[i][j])) {
-                        var val = $_.findMin(arguments[i][j]);
+        for(var i = 0; i < a.length; i++) {
+            if($_.isArray(a[i])) {
+                for(var j = 0; j < a[i].length; j++) {
+                    if($_.isArray(a[i][j])) {
+                        var val = $_.findMin(a[i][j]);
                         if(val < min) {
                             min = val;
                         }
                     } else {
-                        if(arguments[i][j] < min) {
-                            min = arguments[i][j];
+                        if(a[i][j] < min) {
+                            min = a[i][j];
                         }
                     }
                 }
-            } else if(arguments[i] < min) {
-                min = arguments[i];
+            } else if(a[i] < min) {
+                min = a[i];
             }
         }
         return min;
     };
 
     $_.flatten = function() {
-        if(arguments === void 0) { return; }
-        var distinct = arguments[arguments.length - 1];
+        var a = arguments;
+        if(a === void 0) { return; }
+        var distinct = a[a.length - 1];
         var hasDistinct = true;
         if(distinct !== true && distinct !== false) {
             distinct = false;
@@ -59,14 +62,14 @@ var $_ = (function($_) {
         }
 
         var arr = [];
-        for(var i = 0; i < arguments.length - (hasDistinct ? 1 : 0); i++) {
-            if($_.isArray(arguments[i])) {
-                for(var j = 0; j < arguments[i].length; j++) {
-                    arr = $_.flatten(arr, arguments[i][j], distinct);
+        for(var i = 0; i < a.length - (hasDistinct ? 1 : 0); i++) {
+            if($_.isArray(a[i])) {
+                for(var j = 0; j < a[i].length; j++) {
+                    arr = $_.flatten(arr, a[i][j], distinct);
                 }
             } else {
-                if(!distinct || (distinct && arr.indexOf(arguments[i]) === -1)) {
-                    arr.push(arguments[i]);
+                if(!distinct || (distinct && arr.indexOf(a[i]) === -1)) {
+                    arr.push(a[i]);
                 }
             }
         }
