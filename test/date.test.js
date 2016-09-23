871f0a84d976e21687e9c9451d92321a063c7b5b
Clint Pamperin <c.pamperin21@gmail.com>
2016-09-22 21:54:20 -0500

    add: sert to date test

--- test/date.test.js
+++ test/date.test.js
@@ -4,7 +4,8 @@ if(!window && require) {
 }
 
 (function dateToString() {
-    var date = new Date('December 4, 2100 06:05:00');
+    var date = new Date('December 3, 1809 06:05:00');
     var str = $_.dateToString(date, 'y yy yyy yyyy t tt s ss M MM MMM MMMM m mm H HH h hh d dd ddd dddd');
-    console.log(str);
+    
+    $_.assert('dateToString()', '9 09 809 1809 A AM 0 00 12 12 Dec December 5 05 6 06 6 06 3 03 Sun Sunday', str);
 })();
