e51337f36bb4dd90a2ff198176268dd5e9e196ab
Clint Pamperin <c.pamperin21@gmail.com>
2016-09-25 20:28:58 -0500

    add: subtract, multiply, divide

--- src/number.js
+++ src/number.js
@@ -42,3 +42,65 @@
         }
         return r;
     };
+
+    /**
+     * Subtract numbers
+     * @param {number} args - Args to subtract
+     */
+    $_.subtract = function() {
+        var a = arguments;
+        if(a === void 0 || a.length < 1) { return; }
+        var r = a[0];
+        for(var i = 1; i < a.length; i++) {
+            if($_.isArray(a[i])) {
+                for(var j = 0; j < a[i].length; j++) {
+                    r = $_.subtract(r, a[i][j]);
+                }
+            } else {
+                r -= a[i];
+            }
+        }
+        return r;
+    };
+
+    /**
+     * Multiply numbers
+     * @param {number} args - Args to multiply
+     */
+    $_.multiply = function() {
+        var a = arguments;
+        if(a === void 0 || a.length < 1) { return; }
+        var r = a[0];
+        for(var i = 1; i < a.length; i++) {
+            if($_.isArray(a[i])) {
+                for(var j = 0; j < a[i].length; j++) {
+                    r = $_.multiply(r, a[i][j]);
+                }
+            } else {
+                r *= a[i];
+            }
+        }
+        return r;
+    };
+
+    /**
+     * Divide numbers
+     * @param {number} args - Args to divide
+     */
+    $_.divide = function() {
+        var a = arguments;
+        if(a === void 0 || a.length < 1) { return; }
+        var r = a[0];
+        for(var i = 1; i < a.length; i++) {
+            if($_.isArray(a[i])) {
+                for(var j = 0; j < a[i].length; j++) {
+                    r = $_.divide(r, a[i][j]);
+                }
+            } else {
+                if(a[i] !== 0) {
+                    r *= a[i];
+                }
+            }
+        }
+        return r;
+    };
